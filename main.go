38c1dd6a007e1913f2a057946d303c6f8c8c7a80
Teddy <SirTeddy99>
2024-06-18 21:58:51 +0200

    starten på go code

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main()  {
+    
+    var nameOne string = "mario"
+    var nameTwo = "luigi"
+    var nameThree string
+
+    
+    
+    
+    fmt.Println(nameOne, nameTwo, nameThree)
+
+    nameOne = "peach"
+    nameThree = "bowser"
+
+
+    
+    fmt.Println(nameOne, nameTwo, nameThree)
+
+
+    nameFour := "yoshi"
+    fmt.Println(nameFour)
+
+    var ageOne int = 20
+    var ageTwo = 30
+    ageThree := 40
+
+    fmt.Println(ageOne, ageTwo, ageThree)
+
+    var numOne int8 = 25
+    var numTwo int8 = -128
+    var numThree uint = 25
+
+
+
+
+}
