61ba31c8a16304db30fdfb65303fae83d8758d67
Teddy <SirTeddy99>
2024-06-21 23:26:35 +0200

    this will do

---
+++ README.md
@@ -0,0 +1,35 @@
+# Test go repo..++ 
+
+
+## Needed a place to get my handy shortcuts
+```text
+ci" removes text inside ""
+ciw removes word
+ca" removes "" when inside
+
+gd go to definition
+
+* searches for word in document like / but for the word your curser is on
+
+<leader>vca autofix with lsp
+
+f" jump to the first "
+F" jump backwords to "
+
+ctrl+d go back after jumping?
+
+-- inside fugitive --
+
+use = to see diff in same window
+
+czz stash changes
+
+ctrl+w + o Will close all windows but the one you are inn
+
+czA will force pop both changes from stash
+
+gu use the diff on left
+gh use the diff on right
+
+<leader>p git push apperently
+```
--- main.go
+++ main.go
@@ -3,39 +3,34 @@ package main
 import "fmt"
 
 func main()  {
+
+    var ages = [3]int{20, 25, 30}
+
+    names := [4]string{"yoshi", "mario", "peach", "bowser"}
+    names[1] = "lugiu"
+
+    fmt.Println(ages, len(ages))
+    fmt.Println(names, len(names))
+
+    // slices
+    var scores = []int{100, 50,  60}
+    scores[2] = 25
+    scores = append(scores, 85)
     
-    var nameOne string = "mario"
-    var nameTwo = "luigi"
-    var nameThree string
+    fmt.Println(scores, len(scores))
 
+    // slices ranges includes first but not last
+    rangeOne := names[1:3]
+    rangeTwo := names[2:]
+    rangeThree := names[:3]
+
+    fmt.Println(rangeOne, rangeThree)
+    fmt.Println(rangeTwo)
     
-    
-    
-    fmt.Println(nameOne, nameTwo, nameThree)
-
-    nameOne = "peach"
-    nameThree = "bowser"
+    rangeOne = append(rangeOne, "koopa")
+    fmt.Println(rangeOne)
 
 
-    
-    fmt.Println(nameOne, nameTwo, nameThree)
 
-
-    nameFour := "yoshi"
-    fmt.Println(nameFour)
-
-    var ageOne int = 20
-    var ageTwo = 30
-    ageThree := 40
-
-    fmt.Println(ageOne, ageTwo, ageThree)
-
-    var numOne int8 = 25
-    var numTwo int8 = -128
-    var numThree uint = 25
-
-
-    fmt.Println(numOne, numTwo, numThree)
-    
 
 }
